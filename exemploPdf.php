4c105e73231a61ceddebd244ce8eb759c3b273d2
Thiago Silva <thiago.dasilvaresende@gmail.com>
2012-07-10 23:23:02 -0300

    Exemplo HTML2PDF

---
+++ exemploPdf.php
@@ -0,0 +1,38 @@
+<?php
+# Aqui incluímos a classe html2pdf.
+include('html2pdf/html2pdf.class.php');
+ 
+$html = '
+<link rel="stylesheet" type="text/css" href="exemploPdf.css" />
+ 
+<div id="logo"></div>
+<span id="texto">HTML2PDF</span>
+
+<table id="table100" border="1">
+	<tr>
+		<td style="width:20%">Table com width</td>
+		<td style="width:80%">100%</td>
+	</tr>
+</table>';
+ 
+# Converte o html para pdf.
+try
+{
+    $html2pdf = new HTML2PDF('P','A4','pt', false, 'ISO-8859-15', 2);
+     
+    # Passamos o html que queremos converte.
+    $html2pdf->writeHTML($html); 
+     
+    /* Exibe o pdf:
+     * 1º parãmetro: Nome do arquivo pdf. O nome que você quer dar ao pdf gerado. 
+     * 2º parâmetro: Tipo de saída: 
+                     I: Abre o pdf gerado no navegador. 
+                     D: Abre a janela para você realizar o download do pdf. 
+                     F: Salva o pdf em alguma pasta do servidor. */
+    $html2pdf->Output('exemploPdf.pdf', 'I');
+}
+catch(HTML2PDF_exception $e) 
+{ 
+	echo $e; 
+}
+?>
