55980306d999782565116352bd2dc0dde54a21e4
Thiago Silva <thiago.dasilvaresende@gmail.com>
2012-07-11 00:21:12 -0300

    Exemplo com imagem gerada dinamicamente.

--- exemploPdf.php
+++ exemploPdf.php
@@ -1,5 +1,5 @@
 <?php
-# Aqui incluímos a classe html2pdf.
+# Aqui incluÃ­mos a classe html2pdf.
 include('html2pdf/html2pdf.class.php');
  
 $html = '
@@ -13,21 +13,23 @@ $html = '
 		<td style="width:20%">Table com width</td>
 		<td style="width:80%">100%</td>
 	</tr>
-</table>';
+</table>
+
+<img src="http://localhost/Convertendo-HTML-para-PDF-Html2Pdf/imagemJpegDinamica.php?cor=1" />';
  
 # Converte o html para pdf.
 try
 {
-    $html2pdf = new HTML2PDF('P','A4','pt', false, 'ISO-8859-15', 2);
+    $html2pdf = new HTML2PDF('P', 'A4', 'pt', false, 'ISO-8859-15', 2);
      
     # Passamos o html que queremos converte.
     $html2pdf->writeHTML($html); 
      
     /* Exibe o pdf:
-     * 1º parãmetro: Nome do arquivo pdf. O nome que você quer dar ao pdf gerado. 
-     * 2º parâmetro: Tipo de saída: 
+     * 1Âº parÃ¢metro: Nome do arquivo pdf. O nome que vocÃª quer dar ao pdf gerado. 
+     * 2Âº parÃ¢metro: Tipo de saÃ­da: 
                      I: Abre o pdf gerado no navegador. 
-                     D: Abre a janela para você realizar o download do pdf. 
+                     D: Abre a janela para vocÃª realizar o download do pdf. 
                      F: Salva o pdf em alguma pasta do servidor. */
     $html2pdf->Output('exemploPdf.pdf', 'I');
 }
@@ -35,4 +37,3 @@ catch(HTML2PDF_exception $e)
 { 
 	echo $e; 
 }
-?>
